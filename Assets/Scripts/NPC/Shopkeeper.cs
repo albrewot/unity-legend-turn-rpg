29b06b21a4c4674844c299720ef4c09c87b0223b
Albert Torres <albrewot@gmail.com>
2020-05-19 16:41:24 -0400

    [Build] Test build 0.0.7
    Progression with the Shop UI and added a Shopkeeper in the town shop.
    
    Changed the build template to be browser responsive.

---
+++ Assets/Scripts/NPC/Shopkeeper.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Shopkeeper : MonoBehaviour
+{
+    [SerializeField] public GameObject interactBox;
+    [SerializeField] public TextMeshProUGUI interactText;
+    [SerializeField] public string interactDialog;
+
+    //Methods
+    public void OpenShop() {
+        if (Input.GetKeyDown(KeyCode.F) && !Shop.instance.shopMenu.gameObject.activeInHierarchy) {
+            Shop.instance.OpenShop();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OpenShop();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Player")) {
+            interactText.text = interactDialog;
+            interactBox.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.CompareTag("Player")) {
+            interactBox.SetActive(false);
+            Shop.instance.CloseShop();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Shop.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Shop : MonoBehaviour {
+    [SerializeField] public static Shop instance;
+    [SerializeField] public GameObject shopMenu;
+    [SerializeField] public GameObject buyWindow;
+    [SerializeField] public GameObject sellWindow;
+    [SerializeField] public GameObject[] windows;
+    [SerializeField] public TextMeshProUGUI goldText;
+
+    //Methods
+    public void OpenShop() {
+        shopMenu.SetActive(true);
+        goldText.text = GameManager.instance.currentGold.ToString() + "g";
+        ToggleWindow(0);
+        PlayerController.instance.currentState = PlayerState.menu;
+    }
+
+    public void CloseShop() {
+        shopMenu.SetActive(false);
+        ToggleWindow(0);
+        PlayerController.instance.currentState = PlayerState.walk;
+    }
+
+    public void ToggleWindow(int windowIndex) {
+        for (int i = 0; i < windows.Length; i++) {
+            if (i == windowIndex) {
+                windows[i].SetActive(true);
+            } else {
+                windows[i].SetActive(false);
+            }
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start() {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+}
